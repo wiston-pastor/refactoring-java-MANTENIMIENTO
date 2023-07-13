4c8eb89811a8dfa1038c3affbafcb2e96113ce64
wiston-pastor <77182864+wiston-pastor@users.noreply.github.com>
2023-07-13 16:16:43 -0500

    Update Main.java
    mejora del codigo smels #1

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,20 @@ public class Main {
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    //se agrego rentalinfo //
+    //para seguir el princicio de responsabilidad unica
+    RentalInfo rentalInfo = new RentalInfo();
+    
+    Customer customer = new Customer("C. U. Stomer", Arrays.asList(
+            new MovieRental("F001", 3),
+            new MovieRental("F002", 1)
+    ));
+    
+    String result = rentalInfo.statement(customer);
 
+    //se acorto el if
     if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        throw new AssertionError("Expected: \n" + expected + "\nGot: \n" + result);
     }
 
     System.out.println("Success");
